63328d69257afb06196c45eb13c46c2ea65b6296
William Addesso <wsaddesso@gmail.com>
2022-10-23 00:55:32 -0400

    Added more functionality.

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ const LinkedList = () => {
   const prepend = (value) => {
     return list.unshift(value);
   };
-  return { list, append, prepend };
+  const size = () => {
+    return list.length;
+  };
+  const head = () => {
+    return list[0];
+  };
+  const tail = () => {
+    return list[list.length - 1];
+  };
+  const at = (index) => {
+    return list[index];
+  };
+  const pop = () => {
+    list.pop();
+  };
+  return { list, append, prepend, size, head, tail, at, pop };
 };
 
 const Node = () => {
@@ -17,8 +32,17 @@ const Node = () => {
   const nextNode = null;
 };
 
-const test = linkedList();
-test.append('1');
-test.prepend(0);
+const test = LinkedList();
+test.append(1);
+test.append(2);
+test.append(3);
 
+console.log(test);
+console.log(test.append(4));
+console.log(test.prepend(0));
+console.log(test.size());
+console.log(test.head());
+console.log(test.tail());
+console.log(test.at(1));
+console.log(test.pop());
 console.log(test);
